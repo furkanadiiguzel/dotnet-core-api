a7030d84d1c19c0393d4e36ad0533cb1cf972236
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-06 13:58:15 +0300

    JWT settings added

--- MyApi/Program.cs
+++ MyApi/Program.cs
@@ -1,25 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+// ConfigureServices metodu
+services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = Configuration["Jwt:Issuer"],
+            ValidAudience = Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+        };
+    });
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+// Configure metodu içinde
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
