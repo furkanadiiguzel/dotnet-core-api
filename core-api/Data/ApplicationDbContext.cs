6e0bd3359d6831769932a10e149cc21103bc9092
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-07 11:07:22 +0300

    configurations

---
+++ core-api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Define any database-specific configurations here
+    }
+}
