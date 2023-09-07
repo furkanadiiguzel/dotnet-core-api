66a61c1eef92a04802ff6facb388c6d067996ccf
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-07 11:36:03 +0300

    starting over

--- core-api/Models/User.cs
+++
@@ -1,8 +0,0 @@
-// Models/User.cs
-public class User
-{
-    public int Id { get; set; }
-    public string Username { get; set; }
-    public string PasswordHash { get; set; }
-    // Add other user-related properties here
-}
