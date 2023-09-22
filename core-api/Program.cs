061fce7bf64ab76f9744475c49fb8d35c5c73b18
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-22 16:50:46 +0300

    commit

--- core-api/Program.cs
+++ core-api/Program.cs
@@ -8,6 +8,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyPolicy",builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("sqlServerConnStr"));
@@ -22,6 +32,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("MyPolicy");
+
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
