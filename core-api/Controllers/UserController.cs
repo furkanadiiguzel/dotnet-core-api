d91dbbd0bff3f34da0cceec00fc4c8aa929d6a58
furkanadiiguzel <furkan.adiguzel@ug.bilkent.edu.tr>
2023-09-28 23:58:32 +0300

    commit

--- core-api/Controllers/UserController.cs
+++ core-api/Controllers/UserController.cs
@@ -1,21 +1,21 @@
-using Microsoft.AspNetCore.Http;
+using core_api.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
-using System.Threading.Tasks;
-using core_api.Context; 
-using core_api.Models; 
-using core_api.Helpers;
-using System.Text;
-using System;
 using System.Text.RegularExpressions;
-using Microsoft.Extensions.Options;
+using System.Text;
+using core_api.Context;
+using Microsoft.EntityFrameworkCore;
+using core_api.Helpers;
+using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
+using System;
 using System.Security.Claims;
-using System.Text.Json;
 using Microsoft.IdentityModel.Tokens;
-
-
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Threading.Tasks;
+using System.Linq;
+using core_api.Models.Dto;
 
 namespace core_api.Controllers
 {
@@ -24,110 +24,176 @@ namespace core_api.Controllers
     public class UserController : ControllerBase
     {
         private readonly AppDbContext _authContext;
-
-        public UserController(AppDbContext appDbContext)
+        public UserController(AppDbContext context)
         {
-            _authContext = appDbContext;
+            _authContext = context;
         }
 
         [HttpPost("authenticate")]
         public async Task<IActionResult> Authenticate([FromBody] User userObj)
         {
             if (userObj == null)
-            {
                 return BadRequest();
-            }
 
-            var user = await _authContext.Users.FirstOrDefaultAsync(u =>
-                u.Username == userObj.Username);
+            var user = await _authContext.Users
+                .FirstOrDefaultAsync(x => x.Username == userObj.Username);
 
             if (user == null)
-            {
-                return NotFound(new { Message = "User not found" });
+                return NotFound(new { Message = "User not found!" });
 
-            }
-            if(!(PasswordHashing.VerifyPassword(userObj.Password,user.Password)))
+            if (!PasswordHasher.VerifyPassword(userObj.Password, user.Password))
             {
-                return BadRequest(new { Message = "Password is incorrect" });
+                return BadRequest(new { Message = "Password is Incorrect" });
             }
 
-            return Ok(new { Message = "Login Success" });
+            user.Token = CreateJwt(user);
+            var newAccessToken = user.Token;
+            var newRefreshToken = CreateRefreshToken();
+            user.RefreshToken = newRefreshToken;
+            user.RefreshTokenExpiryTime = DateTime.Now.AddDays(5);
+            await _authContext.SaveChangesAsync();
+
+            return Ok(new TokenApiDto()
+            {
+                AccessToken = newAccessToken,
+                RefreshToken = newRefreshToken
+            });
         }
 
         [HttpPost("register")]
-        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        public async Task<IActionResult> AddUser([FromBody] User userObj)
         {
             if (userObj == null)
-            {
                 return BadRequest();
-            }
 
-            var user = await _authContext.Users.FirstOrDefaultAsync(u => u.Username == userObj.Username);
+            // check email
+            if (await CheckEmailExistAsync(userObj.Email))
+                return BadRequest(new { Message = "Email Already Exist" });
 
-            if (user != null)
-            {
-                return BadRequest(new { Message = "User already exists" });
-            }
-            //Check Username
-            if(await CheckUserNameExistAsync(userObj.Username)){
-                return BadRequest(new { Message = "Username already exists" });
-            }
-            //Check Email
-            if(await CheckEmailExistAsync(userObj.Email)){
-                return BadRequest(new { Message = "Email already exists" });
-            }
+            //check username
+            if (await CheckUsernameExistAsync(userObj.Username))
+                return BadRequest(new { Message = "Username Already Exist" });
 
-            //Check Password Strenght
+            var passMessage = CheckPasswordStrength(userObj.Password);
+            if (!string.IsNullOrEmpty(passMessage))
+                return BadRequest(new { Message = passMessage.ToString() });
 
-            var pass = CheckPasswordStrength(userObj.Password);
-            if(!string.IsNullOrEmpty(pass)){
-                return BadRequest(new { Message = pass.ToString() });
-            }
-
-
-
-
-            userObj.Password = PasswordHashing.HashPassword(userObj.Password);
+            userObj.Password = PasswordHasher.HashPassword(userObj.Password);
             userObj.Role = "User";
             userObj.Token = "";
-            _authContext.Users.Add(userObj);
+            await _authContext.AddAsync(userObj);
             await _authContext.SaveChangesAsync();
-
-            return Ok(new { Message = "User created successfully" });
-
-            
+            return Ok(new
+            {
+                Status = 200,
+                Message = "User Added!"
+            });
         }
-        private Task<bool> CheckUserNameExistAsync(string username)
-            => _authContext.Users.AnyAsync(u=>u.Username==username);
 
-        private Task<bool> CheckEmailExistAsync(string email)
-            => _authContext.Users.AnyAsync(u=>u.Email==email);
+        private Task<bool> CheckEmailExistAsync(string? email)
+            => _authContext.Users.AnyAsync(x => x.Email == email);
 
-        private string CheckPasswordStrength(string password){
+        private Task<bool> CheckUsernameExistAsync(string? username)
+            => _authContext.Users.AnyAsync(x => x.Email == username);
+
+        private static string CheckPasswordStrength(string pass)
+        {
             StringBuilder sb = new StringBuilder();
-            if(password.Length < 8 ){
-                sb.Append("Password must be at least 8 characters long"+Environment.NewLine);
-            }
-            if(!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]")))
-            {
-                sb.Append("Password must contain at least one uppercase and lowercase letter"+Environment.NewLine);
-            }
-            if(Regex.IsMatch(password,"[0-9]"))
-            {
-                sb.Append("Password must contain at least one number"+Environment.NewLine);
-            }
-           
+            if (pass.Length < 9)
+                sb.Append("Minimum password length should be 8" + Environment.NewLine);
+            if (!(Regex.IsMatch(pass, "[a-z]") && Regex.IsMatch(pass, "[A-Z]") && Regex.IsMatch(pass, "[0-9]")))
+                sb.Append("Password should be AlphaNumeric" + Environment.NewLine);
+            if (!Regex.IsMatch(pass, "[<,>,@,!,#,$,%,^,&,*,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,~,`,-,=]"))
+                sb.Append("Password should contain special charcter" + Environment.NewLine);
             return sb.ToString();
         }
-        [HttpGet]
-        public async Task<IActionResult> GetAllUsers()
+
+        private string CreateJwt(User user)
         {
-            var users = await _authContext.Users.ToListAsync();
-            return Ok(users);
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverysceret.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Name,$"{user.Username}")
+            });
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddSeconds(10),
+                SigningCredentials = credentials
+            };
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
         }
-        
 
+        private string CreateRefreshToken()
+        {
+            var tokenBytes = RandomNumberGenerator.GetBytes(64);
+            var refreshToken = Convert.ToBase64String(tokenBytes);
 
-        
+            var tokenInUser = _authContext.Users
+                .Any(a => a.RefreshToken == refreshToken);
+            if (tokenInUser)
+            {
+                return CreateRefreshToken();
+            }
+            return refreshToken;
+        }
+
+        private ClaimsPrincipal GetPrincipleFromExpiredToken(string token)
+        {
+            var key = Encoding.ASCII.GetBytes("veryverysceret.....");
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateLifetime = false
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token,tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("This is Invalid Token");
+            return principal;
+                
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUsers()
+        {
+            return Ok(await _authContext.Users.ToListAsync());
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh([FromBody]TokenApiDto tokenApiDto)
+        {
+            if (tokenApiDto is null)
+                return BadRequest("Invalid Client Request");
+            string accessToken = tokenApiDto.AccessToken;
+            string refreshToken = tokenApiDto.RefreshToken;
+            var principal = GetPrincipleFromExpiredToken(accessToken);
+            var username = principal.Identity.Name;
+            var user = await _authContext.Users.FirstOrDefaultAsync(u => u.Username == username);
+            if (user is null || user.RefreshToken != refreshToken || user.RefreshTokenExpiryTime <= DateTime.Now)
+                return BadRequest("Invalid Request");
+            var newAccessToken = CreateJwt(user);
+            var newRefreshToken = CreateRefreshToken();
+            user.RefreshToken = newRefreshToken;
+            await _authContext.SaveChangesAsync();
+            return Ok(new TokenApiDto()
+            {
+                AccessToken = newAccessToken,
+                RefreshToken = newRefreshToken,
+            });
+        }
     }
+
 }
--- core-api/Helpers/PasswordHashing.cs
+++ core-api/Helpers/PasswordHashing.cs
@@ -1,53 +1,47 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Security.Cryptography;
-
 
 namespace core_api.Helpers
 {
-    public class PasswordHashing
+    public class PasswordHasher
     {
-        private static RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
         private static readonly int SaltSize = 16;
         private static readonly int HashSize = 20;
+        private static readonly int Iterations = 10000;
 
-        private static readonly int Iteration = 10000;
+        public static string HashPassword(string password)
+        {
+            byte[] salt;
+            rngCsp.GetBytes(salt = new byte[SaltSize]);
 
-        public static string HashPassword(string password){
-            byte[] salt;;
-            rng.GetBytes(salt = new byte[SaltSize]);
-            var key= new Rfc2898DeriveBytes(password,salt,Iteration);
+            var key = new Rfc2898DeriveBytes(password, salt, Iterations);
             var hash = key.GetBytes(HashSize);
 
             var hashBytes = new byte[SaltSize + HashSize];
-            Array.Copy(salt,0,hashBytes,0,SaltSize);
-            Array.Copy(hash,0,hashBytes,SaltSize,HashSize);
+            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
 
             var base64Hash = Convert.ToBase64String(hashBytes);
 
-
             return base64Hash;
-
         }
-        public static bool VerifyPassword(string password, string base64Hash){
+
+        public static bool VerifyPassword(string password, string base64Hash)
+        {
             var hashBytes = Convert.FromBase64String(base64Hash);
+
             var salt = new byte[SaltSize];
-            Array.Copy(hashBytes,0,salt,0,SaltSize);
-            var key = new Rfc2898DeriveBytes(password,salt,Iteration);
+            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
+
+            var key = new Rfc2898DeriveBytes(password, salt, Iterations);
             byte[] hash = key.GetBytes(HashSize);
 
-            for(int i=0;i<HashSize;i++){
-                if(hashBytes[i+SaltSize]!=hash[i]){
+            for (var i = 0; i < HashSize; i++)
+            {
+                if (hashBytes[i + SaltSize] != hash[i])
                     return false;
-                }
             }
             return true;
-
-
         }
-
-        
     }
 }
